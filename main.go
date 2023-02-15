5b2cdb1ea973fe05772808795dc15ae066ead0c7
Nikolay <nickchirgin@gmail.com>
2023-02-15 13:12:29 +0300

    added valid sudoku solution

--- main.go
+++ main.go
@@ -3,10 +3,8 @@ package main
 import (
 	"fmt"
 
-	"github.com/nickchirgin/leetcode/selectionSort"
+	"github.com/nickchirgin/leetcode/validSudoku"
 )
 func main() {
-	test := []int{2, 7, 11 ,15, 17, 18, 19, 20, 2, 7, 7}
-
-	fmt.Println(selectionSort.SelectionSort(test))
+	fmt.Println(validSudoku.IsValidSudoku())
 }
