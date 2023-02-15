2f7cb703e44616fab2318a0e3157f3d765b334e7
Nikolay <nickchirgin@gmail.com>
2023-02-15 18:04:55 +0300

    added twosum two pointers solution

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 
+	"github.com/nickchirgin/leetcode/twosum"
 	"github.com/nickchirgin/leetcode/validPalyndrome"
 )
 func main() {
+	nums := []int{2, 7, 11, 15, 25}
 	fmt.Println(validPalyndrome.ValidPalidrome("A man, a plan, a canal: Panama"))
+	fmt.Println(twosum.TwoSumPointer(nums, 26))
 }
