248cfa1a3d6fde72e8a93af4645eaf88fe34f648
Nikolay <nickchirgin@gmail.com>
2023-02-16 17:21:43 +0300

    added valid parenthesis problem solution

--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 
 	"github.com/nickchirgin/leetcode/maxArea"
+	"github.com/nickchirgin/leetcode/validParenthesis"
 )
 func main() {
 	nums := []int{1,3,2,5,25,24,5}
 	fmt.Println(maxArea.MaxArea(nums))
+	fmt.Println(validParenthesis.ValidParenthesis("()[]{}"))
 }
