aea1cdd058a6721365f62d15a2d86bca385ed94c
Nikolay <nickchirgin@gmail.com>
2023-05-14 16:48:19 +0300

    added more simplistic version of the previous problem

--- minstack/asteroid.go
+++ minstack/asteroid.go
@@ -1,40 +1,23 @@
 package minstack
 
-import "math"
-
 func AsteroidCollision(asteroids []int) []int {
- stack := []int{}
- for _, asteroid := range asteroids {
-	last := len(stack) - 1
-	if len(stack) == 0  ||  !isOpposite(stack[last], asteroid){
-		stack = append(stack, asteroid)
-		continue
+	stack := []int{}
+	for _, asteroid := range asteroids {
+	 for len(stack) > 0 && asteroid < 0 && stack[len(stack)-1] > 0 {
+		 last := len(stack) - 1
+		 diff := asteroid + stack[last]
+		 if diff < 0 {
+			 stack = stack[:last]
+		 } else if diff > 0 {
+			 asteroid = 0
+		 } else {
+			 asteroid = 0
+			 stack = stack[:last]
+		 }
+	 }
+	 if asteroid != 0 {
+		 stack = append(stack, asteroid)
+	 }
 	}
-	if stack[last] == asteroid * -1 {
-		stack = stack[:last]
-		continue
-	}
-	temp := asteroid
-	for last >= 0 && isOpposite(stack[last], temp) {
-		temp = max(stack[last], temp)
-		stack[last] = temp
-		last--
-	}
-	stack = stack[:last+1]
-	stack = append(stack, temp)
+	return stack
  }
- return stack
-}
-
-
-
-func isOpposite(a, b int) bool {
-	return (a > 0 && b < 0) || (b > 0 && a < 0)
-}
-
-func max(a, b int) int {
-	if math.Abs(float64(a)) > math.Abs(float64(b)) {
-		return a
-	}
-	return b
-}
